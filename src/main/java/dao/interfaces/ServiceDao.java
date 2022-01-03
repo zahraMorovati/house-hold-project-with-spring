8ced183425f2c17a0827cdf0c39c45ab1ac07290
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 19:49:47 +0330

    add interface ServiceDao.java

---
+++ src/main/java/dao/interfaces/ServiceDao.java
@@ -0,0 +1,14 @@
+package dao.interfaces;
+
+import model.entity.Service;
+
+
+import java.util.List;
+
+public interface ServiceDao {
+    void save(Service service);
+    void delete(Service service);
+    void update(Service service);
+    List<Service> getServiceById(int id);
+    List<Service> getAllServices();
+}
