8cba31e824274efcfb6f05c587a421604e993c35
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 19:42:01 +0330

    add class CustomerDaoImpl.java

---
+++ src/main/java/dao/CustomerDaoImpl.java
@@ -0,0 +1,33 @@
+package dao;
+
+import dao.interfaces.CustomerDao;
+import model.entity.Customer;
+
+import java.util.List;
+
+public class CustomerDaoImpl implements CustomerDao {
+    @Override
+    public void save(Customer customer) {
+
+    }
+
+    @Override
+    public void delete(Customer customer) {
+
+    }
+
+    @Override
+    public void update(Customer customer) {
+
+    }
+
+    @Override
+    public List<Customer> getCustomerById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return null;
+    }
+}
