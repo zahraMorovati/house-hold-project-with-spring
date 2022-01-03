4653f9e6d6d1b43b9677650aee1fab8e1e091b06
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 19:50:07 +0330

    add interface SpecialistDao.java

---
+++ src/main/java/dao/interfaces/SpecialistDao.java
@@ -0,0 +1,13 @@
+package dao.interfaces;
+
+import model.entity.Specialist;
+
+import java.util.List;
+
+public interface SpecialistDao {
+    void save(Specialist specialist);
+    void delete(Specialist specialist);
+    void update(Specialist specialist);
+    List<Specialist> getSpecialistById(int id);
+    List<Specialist> getAllSpecialists();
+}
