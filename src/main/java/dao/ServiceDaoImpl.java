4fec314305f05d64b807d129b4946a5c0f07a8ed
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 19:48:43 +0330

    add class ServiceDaoImpl.java

---
+++ src/main/java/dao/ServiceDaoImpl.java
@@ -0,0 +1,33 @@
+package dao;
+
+import dao.interfaces.ServiceDao;
+import model.entity.Service;
+
+import java.util.List;
+
+public class ServiceDaoImpl implements ServiceDao {
+    @Override
+    public void save(Service service) {
+
+    }
+
+    @Override
+    public void delete(Service service) {
+
+    }
+
+    @Override
+    public void update(Service service) {
+
+    }
+
+    @Override
+    public List<Service> getServiceById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Service> getAllServices() {
+        return null;
+    }
+}
