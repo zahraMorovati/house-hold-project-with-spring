6ebaba68c1b9137784e6b5b5150ab2bd329ab07b
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 20:20:55 +0330

    modify OrderDaoImpl.java ---> implements from OrderDao.java

--- src/main/java/dao/OrderDaoImpl.java
+++ src/main/java/dao/OrderDaoImpl.java
@@ -1,4 +1,29 @@
 package dao;
 
-public class OrderDaoImpl {
+import dao.interfaces.OrderDao;
+import model.entity.Customer;
+import model.entity.Order;
+
+import java.util.List;
+
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public void save(Order order) {
+
+    }
+
+    @Override
+    public void delete(Order order) {
+
+    }
+
+    @Override
+    public void update(Order order) {
+
+    }
+
+    @Override
+    public List<Order> getCustomerOrder(Customer customer) {
+        return null;
+    }
 }
