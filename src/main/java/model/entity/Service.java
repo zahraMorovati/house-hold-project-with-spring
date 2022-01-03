9435b0c5c5e6a7db4ad5c33b82b6ae2e6b6a4c39
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 19:48:51 +0330

    add class Service.java

---
+++ src/main/java/model/entity/Service.java
@@ -0,0 +1,37 @@
+package model.entity;
+
+import lombok.*;
+import model.enums.SubService;
+import org.hibernate.Hibernate;
+import javax.persistence.*;
+import java.util.Objects;
+@Builder
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @Column(unique = true)
+    private SubService subService;
+    private double price;
+    @Lob
+    private String explanations; //توضیحات
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Service service = (Service) o;
+        return id != 0 && Objects.equals(id, service.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
