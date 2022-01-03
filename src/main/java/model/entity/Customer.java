16d94d52dca8bb8c1d5ddf5d60089c7b7bd9ca1a
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 19:41:44 +0330

    add class Customer.java

---
+++ src/main/java/model/entity/Customer.java
@@ -0,0 +1,36 @@
+package model.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+@Builder
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+public class Customer extends User {
+
+    private double balance;
+    @OneToMany
+    @ToString.Exclude
+    private List<Order> orders = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Customer customer = (Customer) o;
+        return getId() != 0 && Objects.equals(getId(), customer.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
