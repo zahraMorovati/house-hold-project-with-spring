328eb526a77ce8c9d132cf73446e107237fa7186
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 19:48:00 +0330

    add enum SubService.java

---
+++ src/main/java/dao/SpecialistDaoImpl.java
@@ -0,0 +1,33 @@
+package dao;
+
+import dao.interfaces.SpecialistDao;
+import model.entity.Specialist;
+
+import java.util.List;
+
+public class SpecialistDaoImpl implements SpecialistDao {
+    @Override
+    public void save(Specialist specialist) {
+
+    }
+
+    @Override
+    public void delete(Specialist specialist) {
+
+    }
+
+    @Override
+    public void update(Specialist specialist) {
+
+    }
+
+    @Override
+    public List<Specialist> getSpecialistById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Specialist> getAllSpecialists() {
+        return null;
+    }
+}
---
+++ src/main/java/model/enums/SubService.java
@@ -0,0 +1,6 @@
+package model.enums;
+
+public enum SubService {
+
+    //todo
+}
