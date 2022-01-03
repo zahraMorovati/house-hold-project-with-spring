af4fddfb07f2f9f6cee8fc2e38d0ddf055f2d045
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 19:44:18 +0330

    add class ManagerDaoImpl.java

---
+++ src/main/java/dao/ManagerDaoImpl.java
@@ -0,0 +1,38 @@
+package dao;
+
+import dao.interfaces.ManagerDao;
+import model.entity.Manager;
+
+import java.util.List;
+
+public class ManagerDaoImpl implements ManagerDao {
+    @Override
+    public void save(Manager manager) {
+
+    }
+
+    @Override
+    public void delete(Manager manager) {
+
+    }
+
+    @Override
+    public void update(Manager manager) {
+
+    }
+
+    @Override
+    public List<Manager> getAllManagers() {
+        return null;
+    }
+
+    @Override
+    public List<Manager> getManagerById() {
+        return null;
+    }
+
+    @Override
+    public List<Manager> getManagerByEmailAndPassword() {
+        return null;
+    }
+}
