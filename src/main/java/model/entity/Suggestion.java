0584c4fae7dd57bcf6ad1bb8c8bf7862ad05cd18
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 19:46:34 +0330

    add enum Suggestion.java

---
+++ src/main/java/model/entity/Suggestion.java
@@ -0,0 +1,40 @@
+package model.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+@Builder
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+public class Suggestion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date date;
+    private double suggestedPrice;
+    private double workHour;
+    @Temporal(value = TemporalType.TIME)
+    private Date startTime;
+    @OneToOne
+    private Specialist specialist;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Suggestion that = (Suggestion) o;
+        return id != 0 && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
