fdd24bf8b4b30ccf29ecd85c91f63c05d45de6a1
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-03 19:41:31 +0330

    add class Comment.java

---
+++ src/main/java/model/entity/Comment.java
@@ -0,0 +1,34 @@
+package model.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+@Builder
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private double point;
+    @Lob
+    private String comment;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Comment comment = (Comment) o;
+        return id != 0 && Objects.equals(id, comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
