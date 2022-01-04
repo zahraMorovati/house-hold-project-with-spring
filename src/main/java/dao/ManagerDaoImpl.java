4c81944579bd7d8770bd5bb06535168a31b80440
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-04 17:08:04 +0330

    add CRUD methods to (ManagerDaoImpl.java) methods

--- src/main/java/dao/ManagerDaoImpl.java
+++ src/main/java/dao/ManagerDaoImpl.java
@@ -2,23 +2,43 @@ package dao;
 
 import dao.interfaces.ManagerDao;
 import model.entity.Manager;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import util.HibernateUtil;
 
 import java.util.List;
 
 public class ManagerDaoImpl implements ManagerDao {
+
+    private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
     @Override
     public void save(Manager manager) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(manager);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void delete(Manager manager) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(manager);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void update(Manager manager) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(manager);
+        transaction.commit();
+        session.close();
     }
 
     @Override
@@ -27,12 +47,29 @@ public class ManagerDaoImpl implements ManagerDao {
     }
 
     @Override
-    public List<Manager> getManagerById() {
-        return null;
+    public List<Manager> getManagerById(int id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<Manager> query = session.createQuery("from Manager m where m.id=:id");
+        query.setParameter("id", id);
+        List<Manager> results = query.getResultList();
+        transaction.commit();
+        session.close();
+        return results;
     }
 
     @Override
-    public List<Manager> getManagerByEmailAndPassword() {
-        return null;
+    public List<Manager> getManagerByEmailAndPassword(String email, String password) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<Manager> query = session.createQuery("from Manager m where m.email=:email and m.password=:password");
+        query.setParameter("email", email);
+        query.setParameter("password", password);
+        List<Manager> results = query.getResultList();
+        transaction.commit();
+        session.close();
+        return results;
     }
+
+
 }
