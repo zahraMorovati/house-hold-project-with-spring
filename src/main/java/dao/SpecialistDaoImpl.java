1bf9a9d45936d284e5d18320b6df4f4d22f81023
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-04 17:09:12 +0330

    add CRUD methods to (SpecialistDaoImpl.java) methods

--- src/main/java/dao/SpecialistDaoImpl.java
+++ src/main/java/dao/SpecialistDaoImpl.java
@@ -2,32 +2,66 @@ package dao;
 
 import dao.interfaces.SpecialistDao;
 import model.entity.Specialist;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import util.HibernateUtil;
 
 import java.util.List;
 
 public class SpecialistDaoImpl implements SpecialistDao {
+
+    private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
     @Override
     public void save(Specialist specialist) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(specialist);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void delete(Specialist specialist) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(specialist);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void update(Specialist specialist) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(specialist);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public List<Specialist> getSpecialistById(int id) {
-        return null;
+
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<Specialist> query = session.createQuery("from Specialist s where s.id=:id");
+        query.setParameter("id", id);
+        List<Specialist> results = query.getResultList();
+        transaction.commit();
+        session.close();
+        return results;
     }
 
     @Override
     public List<Specialist> getAllSpecialists() {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<Specialist> query = session.createQuery("from Specialist ");
+        List<Specialist> results = query.getResultList();
+        transaction.commit();
+        session.close();
+        return results;
     }
 }
