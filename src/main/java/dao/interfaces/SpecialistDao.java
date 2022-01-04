b5edbdb0f6c4672b3179e43b42928ae69e01058c
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-04 17:11:49 +0330

    sort SpecialistDao.java

--- src/main/java/dao/interfaces/SpecialistDao.java
+++ src/main/java/dao/interfaces/SpecialistDao.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface SpecialistDao {
     void save(Specialist specialist);
+
     void delete(Specialist specialist);
+
     void update(Specialist specialist);
+
     List<Specialist> getSpecialistById(int id);
+
     List<Specialist> getAllSpecialists();
 }
