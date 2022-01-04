2697f461fcf2c639a73abacb199679e5fccbd8c5
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-04 17:05:45 +0330

    add method addOrder to (CustomerDao.java)

--- src/main/java/dao/interfaces/CustomerDao.java
+++ src/main/java/dao/interfaces/CustomerDao.java
@@ -1,16 +1,23 @@
 package dao.interfaces;
 
 import model.entity.Customer;
+import model.entity.Order;
 
 import java.util.List;
 
 public interface CustomerDao {
 
-     void save(Customer customer);
-     void delete(Customer customer);
-     void update(Customer customer);
-     List<Customer> getCustomerById(int id);
-     List<Customer> getAllCustomers();
+    void save(Customer customer);
+
+    void delete(Customer customer);
+
+    void update(Customer customer);
+
+    List<Customer> getCustomerById(int id);
+
+    List<Customer> getAllCustomers();
+
+    void addOrder(Customer customer, Order order);
 
 
 }
