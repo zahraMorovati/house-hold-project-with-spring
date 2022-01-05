8d11b73152369e43c6ec55b87223d41b363d72aa
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-05 16:59:48 +0330

    add OrderService bean

--- src/main/java/config/OrderConfig.java
+++ src/main/java/config/OrderConfig.java
@@ -3,6 +3,7 @@ package config;
 import dao.OrderDaoImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import service.OrderService;
 
 @Configuration
 public class OrderConfig {
@@ -10,5 +11,12 @@ public class OrderConfig {
     @Bean
     public OrderDaoImpl orderDao(){
         return new OrderDaoImpl();
+    }
+
+    @Bean
+    public OrderService orderService(OrderDaoImpl orderDao){
+        OrderService orderService = new OrderService();
+        orderService.setOrderDao(orderDao);
+        return orderService;
     }
 }
