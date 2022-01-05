8bc5feacecfe8c71acfb11023e3972815af335a2
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-05 22:59:43 +0330

    add homeService bean

--- src/main/java/config/HomeServiceConfig.java
+++ src/main/java/config/HomeServiceConfig.java
@@ -3,11 +3,18 @@ package config;
 
 import dao.HomeServiceDaoImpl;
 import org.springframework.context.annotation.Configuration;
+import service.HomeServiceService;
 
 @Configuration
 public class HomeServiceConfig {
 
-    public HomeServiceDaoImpl serviceDao(){
+    public HomeServiceDaoImpl HomeServiceDao(){
         return new HomeServiceDaoImpl();
+    }
+
+    public HomeServiceService homeServiceService(HomeServiceDaoImpl homeServiceDao){
+        HomeServiceService homeServiceService = new HomeServiceService();
+        homeServiceService.setHomeServiceDao(homeServiceDao);
+        return homeServiceService;
     }
 }
