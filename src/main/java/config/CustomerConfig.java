da43edd305f29f298a33fbf94980ba80066e08ce
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-05 16:59:01 +0330

    add CustomerService bean

--- src/main/java/config/CustomerConfig.java
+++ src/main/java/config/CustomerConfig.java
@@ -3,6 +3,7 @@ package config;
 import dao.CustomerDaoImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import service.CustomerService;
 
 @Configuration
 public class CustomerConfig {
@@ -10,6 +11,13 @@ public class CustomerConfig {
     @Bean
     public CustomerDaoImpl customerDao(){
         return new CustomerDaoImpl();
+    }
+
+    @Bean
+    public CustomerService customerService(CustomerDaoImpl customerDao){
+        CustomerService customerService = new CustomerService();
+        customerService.setCustomerDao(customerDao);
+        return customerService;
     }
 
 
