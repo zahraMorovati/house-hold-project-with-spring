a3f5ad5dfd1b82c59bce43f3d09a6bf55496941a
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-05 17:00:10 +0330

    add SpecialistService bean

--- src/main/java/config/SpecialistConfig.java
+++ src/main/java/config/SpecialistConfig.java
@@ -1,8 +1,10 @@
 package config;
 
 import dao.SpecialistDaoImpl;
+import dao.interfaces.SpecialistDao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import service.SpecialistService;
 
 @Configuration
 public class SpecialistConfig {
@@ -10,5 +12,14 @@ public class SpecialistConfig {
     @Bean
     public SpecialistDaoImpl specialistDao(){
         return new SpecialistDaoImpl();
+    }
+
+    @Bean
+    public SpecialistService specialistService(SpecialistDaoImpl specialistDao){
+
+        SpecialistService specialistService = new SpecialistService();
+        specialistService.setSpecialistDao(specialistDao);
+        return specialistService;
+
     }
 }
