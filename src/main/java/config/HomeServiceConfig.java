96589fb788b3b905361fa93809d0127741a951a1
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-06 04:09:09 +0330

    add @Bean in HomeServiceConfig.java

--- src/main/java/config/HomeServiceConfig.java
+++ src/main/java/config/HomeServiceConfig.java
@@ -2,16 +2,20 @@ package config;
 
 
 import dao.HomeServiceDaoImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import service.HomeServiceService;
-
+@Import(value = {SpecialistConfig.class})
 @Configuration
 public class HomeServiceConfig {
 
+    @Bean
     public HomeServiceDaoImpl HomeServiceDao(){
         return new HomeServiceDaoImpl();
     }
 
+    @Bean
     public HomeServiceService homeServiceService(HomeServiceDaoImpl homeServiceDao){
         HomeServiceService homeServiceService = new HomeServiceService();
         homeServiceService.setHomeServiceDao(homeServiceDao);
