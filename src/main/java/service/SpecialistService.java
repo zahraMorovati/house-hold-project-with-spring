4ab0ba33acf2c2f37369b1040f3cd678a0023ea7
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-06 04:12:25 +0330

    add method filter and changePass to SpecialistService.java

--- src/main/java/service/SpecialistService.java
+++ src/main/java/service/SpecialistService.java
@@ -1,6 +1,7 @@
 package service;
 
 import dao.SpecialistDaoImpl;
+import dto.UserDto;
 import model.entity.Specialist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,10 +13,6 @@ import java.util.List;
 public class SpecialistService {
 
     SpecialistDaoImpl specialistDao;
-
-    public void sayHello(){
-        System.out.println("hello");
-    }
 
     public SpecialistDaoImpl getSpecialistDao() {
         return specialistDao;
@@ -45,10 +42,29 @@ public class SpecialistService {
         else return specialistList.get(0);
     }
 
-    public Specialist findSpecialistByEmailAndPassword(String email , String password){
-        List<Specialist> specialists = specialistDao.getSpecialistByEmailAndPassword(email, password);
-        if(specialists.isEmpty())
-            throw new RuntimeException("wrong email or password!");
-        else return specialists.get(0);
+    public void changePassword(String email,String newPass){
+        List<Specialist> result = specialistDao.getSpecialistByEmail(email);
+        if(!result.isEmpty()){
+            Specialist specialist = result.get(0);
+            specialist.setPassword(newPass);
+            specialistDao.update(specialist);
+        }else throw new RuntimeException("wrong email!");
     }
+
+    public List<UserDto> filter(String name , String password , String email){
+        //pagination is not handled that's why i put zero for max result
+        List<UserDto> list = specialistDao.filter(name, email, password, 0, 0);
+        if(list.isEmpty())
+            throw new RuntimeException("cannot find any user!");
+        else return list;
+    }
+
+    public List<Specialist> getAllSpecialists(){
+        List<Specialist> list = specialistDao.getAllSpecialists();
+        if(list.isEmpty())
+            throw new RuntimeException("there is no specialist!");
+        else return list;
+    }
+
+
 }
