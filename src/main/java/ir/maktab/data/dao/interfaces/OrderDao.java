8eec731d2691acc649ca3c53f9f57632ad12d9c4
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-09 18:24:44 +0330

    modify OrderDaoImpl.java to OrderDao.java with spring-data

--- src/main/java/ir/maktab/data/dao/interfaces/OrderDao.java
+++ src/main/java/ir/maktab/data/dao/interfaces/OrderDao.java
@@ -1,13 +1,32 @@
 package ir.maktab.data.dao.interfaces;
 
-import java.util.List;
+import ir.maktab.data.model.entity.Address;
+import ir.maktab.data.model.entity.Order;
+import ir.maktab.data.model.entity.SubService;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface OrderDao {
-    void save(Order order);
+import java.util.Date;
 
-    void delete(Order order);
+@Repository
+public interface OrderDao extends PagingAndSortingRepository<Order, Integer> {
 
-    void update(Order order);
-
-    List<Order> getCustomerOrder(Customer customer);
+    @Transactional
+    @Modifying
+    @Query(value = "update Order o set " +
+            "o.subService=:subService ," +
+            "o.suggestedPrice=:suggestedPrice , " +
+            "o.explanations=:explanations ," +
+            "o.startDate=:startDate ," +
+            "o.address=:address where o.id=:id")
+    void update(@Param("subService") SubService subService,
+                @Param("suggestedPrice") double suggestedPrice,
+                @Param("explanations") String explanations,
+                @Param("startDate") Date startDate,
+                @Param("address") Address address,
+                @Param("id") int id);
 }
