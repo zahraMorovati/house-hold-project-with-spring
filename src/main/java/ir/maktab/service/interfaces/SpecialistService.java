9221be8f77006825fcaeb121483c90349ab3b01f
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-09 20:43:13 +0330

    add filterByNameOrFamilyOrEmail to SpecialistService interface

--- src/main/java/ir/maktab/service/interfaces/SpecialistService.java
+++ src/main/java/ir/maktab/service/interfaces/SpecialistService.java
@@ -2,6 +2,8 @@ package ir.maktab.service.interfaces;
 
 import ir.maktab.data.model.entity.Specialist;
 
+import java.util.List;
+
 public interface SpecialistService {
 
     void save(Specialist specialist);
@@ -10,9 +12,11 @@ public interface SpecialistService {
 
     void update(Specialist specialist);
 
-    Iterable<Specialist> findAll();
+    Iterable<Specialist> findAll(int page , int size);
 
     void changePassword(String email,String newPassword);
 
     Specialist findById(int id);
+
+    List<Specialist> filterByNameOrFamilyOrEmail(String name, String family, String email);
 }
