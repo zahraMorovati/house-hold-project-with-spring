f07c1c6686d8e3927d8b158d710573eeba0e9ca8
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-09 20:38:25 +0330

    add filterByNameOrFamilyOrEmail method to CustomerService.java

--- src/main/java/ir/maktab/service/interfaces/CustomerService.java
+++ src/main/java/ir/maktab/service/interfaces/CustomerService.java
@@ -2,6 +2,8 @@ package ir.maktab.service.interfaces;
 
 import ir.maktab.data.model.entity.Customer;
 
+import java.util.List;
+
 public interface CustomerService {
 
     void save(Customer customer);
@@ -10,11 +12,13 @@ public interface CustomerService {
 
     void update(Customer customer);
 
-    Iterable<Customer> findAll();
+    Iterable<Customer> findAll(int page , int size);
 
     void changePassword(String email,String newPassword);
 
     Customer findById(int id);
+
+    List<Customer> filterByNameOrFamilyOrEmail(String name,String family,String email);
 
 
 }
