80e6ce3dcbbae0fb9aace35fd8b22159f5f766b5
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-09 20:48:18 +0330

    add exception to findById() in OrderServiceImpl.java

--- src/main/java/ir/maktab/service/OrderServiceImpl.java
+++ src/main/java/ir/maktab/service/OrderServiceImpl.java
@@ -2,9 +2,14 @@ package ir.maktab.service;
 
 import ir.maktab.data.dao.interfaces.OrderDao;
 import ir.maktab.data.model.entity.Order;
+import ir.maktab.exception.orderExceptions.OrderNotFoundException;
 import ir.maktab.service.interfaces.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -32,12 +37,15 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
-    public Iterable<Order> findAll() {
-        return orderDao.findAll();
+    public Iterable<Order> findAll(int page , int size) {
+        return orderDao.findAll(PageRequest.of(page,size));
     }
 
     @Override
     public Order findById(int id) {
-        return orderDao.findById(id).orElse(null);
+        Optional<Order> optionalOrder = orderDao.findById(id);
+        if(optionalOrder.isPresent())
+            return optionalOrder.get();
+        else throw new OrderNotFoundException();
     }
 }
