170ae961be9296434a124ae730cb1106bd7840f3
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-09 20:37:25 +0330

    add SubServiceNotFoundException.java

---
+++ src/main/java/ir/maktab/exception/subServiceExceptions/SubServiceNotFoundException.java
@@ -0,0 +1,24 @@
+package ir.maktab.exception.subServiceExceptions;
+/**
+ * Thrown when application cannot find {@code SubService} in database
+ */
+public class SubServiceNotFoundException extends RuntimeException{
+
+
+    /**
+     * Constructs a {@code SubServiceNotFoundException} with no detail message.
+     */
+    public SubServiceNotFoundException() {
+        super();
+    }
+
+    /**
+     * Constructs a {@code SubServiceNotFoundException} with the specified
+     * detail message.
+     *
+     * @param   s   the detail message.
+     */
+    public SubServiceNotFoundException(String s) {
+        super(s);
+    }
+}
