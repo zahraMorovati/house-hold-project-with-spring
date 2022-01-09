ee687343e26349fbd469987995f246676b94df0d
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-09 20:49:34 +0330

    add exception to findById() method in ServiceServiceImpl.java

--- src/main/java/ir/maktab/service/ServiceServiceImpl.java
+++ src/main/java/ir/maktab/service/ServiceServiceImpl.java
@@ -1,12 +1,13 @@
 package ir.maktab.service;
 
 import ir.maktab.data.dao.interfaces.ServiceDao;
-import ir.maktab.data.dao.interfaces.SpecialistDao;
 import ir.maktab.data.model.entity.Service;
+import ir.maktab.exception.serviceExceptions.ServiceNotFoundException;
 import ir.maktab.service.interfaces.ServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import java.util.Optional;
 
-import java.util.List;
 
 @org.springframework.stereotype.Service
 public class ServiceServiceImpl implements ServiceService {
@@ -29,13 +30,16 @@ public class ServiceServiceImpl implements ServiceService {
     }
 
     @Override
-    public Iterable<Service> findAll() {
-        return serviceDao.findAll();
+    public Iterable<Service> findAll(int page , int size) {
+        return serviceDao.findAll(PageRequest.of(page,size));
     }
 
     @Override
     public Service findById(int id) {
-        return serviceDao.findById(id).orElse(null);
+        Optional<Service> optionalService = serviceDao.findById(id);
+        if(optionalService.isPresent())
+            return optionalService.get();
+        else throw new ServiceNotFoundException();
     }
 
     @Override
