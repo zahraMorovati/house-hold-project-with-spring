3e49457b95f0682e5bdc5c36f7a4f831c7fe0206
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-13 01:00:08 +0330

    rename homeService to service in ServiceService.java

--- src/main/java/ir/maktab/service/interfaces/ServiceService.java
+++ src/main/java/ir/maktab/service/interfaces/ServiceService.java
@@ -4,11 +4,11 @@ import ir.maktab.data.model.entity.Service;
 
 public interface ServiceService {
 
-    void save(Service homeService);
+    void save(Service service);
 
-    void delete(Service homeService);
+    void delete(Service service);
 
-    void update(Service homeService);
+    void update(Service service);
 
     Iterable<Service> findAll(int page , int size);
 
