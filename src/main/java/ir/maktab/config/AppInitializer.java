d8660387e94d2a78515b26c9a4efc1fde83b1169
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-24 08:10:03 +0330

    add AppInitializer.java

---
+++ src/main/java/ir/maktab/config/AppInitializer.java
@@ -0,0 +1,27 @@
+package ir.maktab.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+                AppContext.class
+        };
+        //return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+                WebMvcConfig.class,
+                AppConfiguration.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
