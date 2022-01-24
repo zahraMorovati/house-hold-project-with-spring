a749fb6b3473f4dfbc1e0e8f8a3fe3d05fa57e82
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-24 08:14:02 +0330

    add OrderDto.java

---
+++ src/main/java/ir/maktab/data/dto/OrderDto.java
@@ -0,0 +1,32 @@
+package ir.maktab.data.dto;
+
+import ir.maktab.data.entity.*;
+import ir.maktab.data.enums.OrderState;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Builder(setterPrefix = "set")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderDto {
+    private String orderCode;
+    private String  subService;
+    private double suggestedPrice;
+    private String explanations;
+    private Date registrationDate;
+    private Date startDate;
+    private String address;
+    private String orderState;
+    private String specialist;
+    private String customer;
+    private String comment;
+    private double point;
+
+}
