21bac396a250ba22a4504cc95712b53012b91a33
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-24 08:13:34 +0330

    add LastViewInterceptor.java

---
+++ src/main/java/ir/maktab/config/LastViewInterceptor.java
@@ -0,0 +1,22 @@
+package ir.maktab.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LastViewInterceptor /*extends HandlerInterceptorAdapter*/ implements HandlerInterceptor {
+
+    public static final String LAST_VIEW_ATTRIBUTE = LastViewInterceptor.class.getName() + ".lastView";
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (modelAndView != null) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute(LAST_VIEW_ATTRIBUTE, modelAndView.getViewName());
+        }
+//        super.postHandle(request, response, handler, modelAndView);
+    }
+}
