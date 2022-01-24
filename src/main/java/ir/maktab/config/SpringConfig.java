259f1772ae9f53e8e728b651ca8eff5411753683
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-01-24 08:29:11 +0330

    rename DatabaseConfig to AppContext

--- src/main/java/ir/maktab/config/SpringConfig.java
+++ src/main/java/ir/maktab/config/SpringConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(value = {DatabaseConfig.class})
+@Import(value = {AppContext.class})
 @ComponentScan(basePackages = "ir.maktab")
 public class SpringConfig {
 }
