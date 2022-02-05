86d3c9e057eb0add9f44371f5679978cc2e0bee8
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-02-05 12:11:41 +0330

    add builder to Comment.java

--- src/main/java/ir/maktab/data/entity/Comment.java
+++ src/main/java/ir/maktab/data/entity/Comment.java
@@ -1,11 +1,17 @@
 package ir.maktab.data.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+@Builder(setterPrefix = "set")
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
 public class Comment {
