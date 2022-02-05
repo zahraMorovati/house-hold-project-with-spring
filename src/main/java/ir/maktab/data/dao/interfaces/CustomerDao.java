5a99913d7f8140e2aa1370c787c02f1bb142affe
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-02-05 12:30:22 +0330

    add filterCustomer by criteria

--- src/main/java/ir/maktab/data/dao/interfaces/CustomerDao.java
+++ src/main/java/ir/maktab/data/dao/interfaces/CustomerDao.java
@@ -2,6 +2,8 @@ package ir.maktab.data.dao.interfaces;
 
 
 import ir.maktab.data.entity.Customer;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -9,11 +11,50 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Repository
-public interface CustomerDao extends PagingAndSortingRepository<Customer, Integer> {
+public interface CustomerDao extends PagingAndSortingRepository<Customer, Integer>, JpaSpecificationExecutor<Customer> {
 
+
+    static Specification<Customer> filterCustomers(String name, String family, String email) {
+        return (root, criteriaQuery, criteriaBuilder) -> {
+            Predicate condition1 = criteriaBuilder.like(root.get("name"), name);
+            Predicate condition2 = criteriaBuilder.like(root.get("family"), family);
+            Predicate condition3 = criteriaBuilder.like(root.get("email"), email);
+            return criteriaBuilder.or(condition1, condition2, condition3);
+        };
+    }
+
+    static Specification<Customer> advancedFilter(String name, String family, String email,
+                                                  Date startingRegistrationDate,
+                                                  Date endingRegistrationDate,
+                                                  int minOrderNumber, int maxOrderNumber) {
+
+        return (root, criteriaQuery, criteriaBuilder) -> {
+
+            CriteriaQuery<Customer> resultCriteria = criteriaBuilder.createQuery(Customer.class);
+            List<Predicate> filterPredicates = new ArrayList<>();
+
+            if (name != null && !name.isEmpty()) {
+                filterPredicates.add(criteriaBuilder.like(root.get("name"), name));
+            } else if (family != null && !family.isEmpty()) {
+                filterPredicates.add(criteriaBuilder.like(root.get("family"), family));
+            } else if (email != null && !email.isEmpty()) {
+                filterPredicates.add(criteriaBuilder.like(root.get("email"), email));
+            } else if (startingRegistrationDate != null && endingRegistrationDate != null) {
+                filterPredicates.add(criteriaBuilder.between(root.get("RegistrationDate"), startingRegistrationDate, endingRegistrationDate));
+            } else if (maxOrderNumber > 1 && minOrderNumber >= 0) {
+                filterPredicates.add(criteriaBuilder.between(criteriaBuilder.size(root.get("orders")), minOrderNumber, maxOrderNumber));
+            }
+            resultCriteria.select(root).where(filterPredicates.toArray(new Predicate[0]));
+            return resultCriteria.getRestriction();
+        };
+    }
 
     List<Customer> findCustomerByEmail(String email);
 
