ee5cc38d86693601cce42d57c9b3758cb74cd9db
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-02-05 12:50:34 +0330

    add findSuggestionByOrder to SuggestionService.java and implement it in SuggestionServiceImpl.java

--- src/main/java/ir/maktab/service/SuggestionServiceImpl.java
+++ src/main/java/ir/maktab/service/SuggestionServiceImpl.java
@@ -3,6 +3,8 @@ package ir.maktab.service;
 import ir.maktab.data.dao.interfaces.OrderDao;
 import ir.maktab.data.dao.interfaces.SpecialistDao;
 import ir.maktab.data.dao.interfaces.SuggestionDao;
+import ir.maktab.data.dto.SuggestionDto;
+import ir.maktab.data.dto.mappers.SuggestionMapper;
 import ir.maktab.data.entity.Order;
 import ir.maktab.data.entity.Specialist;
 import ir.maktab.data.entity.Suggestion;
@@ -13,12 +15,12 @@ import ir.maktab.exception.suggestionExceptions.SuggestedPriceIsHigherThanBasePr
 import ir.maktab.exception.suggestionExceptions.SuggestionNotFoundException;
 import ir.maktab.service.interfaces.SuggestionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class SuggestionServiceImpl implements SuggestionService {
@@ -36,26 +38,26 @@ public class SuggestionServiceImpl implements SuggestionService {
     }
 
     @Override
-    public void addSpecialistSuggestion(Order order, Specialist specialist, double suggestedPrice,
+    public void addSpecialistSuggestion(String orderCode, String specialistEmail, double suggestedPrice,
                                         double workHour, Date startTime) {
 
-        /* check order existence */
-        List<Order> optionalOrder = orderDao.findOrderByOrderCode(order.getOrderCode());
-        if (!optionalOrder.isEmpty()) {
-            /* check specialist existence */
-            //todo check if orderStatus is not WAITING_FOR_SPECIALIST_TO_COME
-            List<Specialist> optionalSpecialist = specialistDao.findSpecialistByEmail(specialist.getEmail());
-            if (!optionalSpecialist.isEmpty()) {
+        List<Order> optionalOrder = orderDao.findOrderByOrderCode(orderCode);
+        if (optionalOrder.size()>=1) {
 
-                order = optionalOrder.get(0);
-                specialist = optionalSpecialist.get(0);
+            List<Specialist> optionalSpecialist = specialistDao.findSpecialistByEmail(specialistEmail);
+            if (optionalSpecialist.size()>=1) {
+
+                Order order = optionalOrder.get(0);
+                Specialist specialist = optionalSpecialist.get(0);
 
                 if (suggestedPrice < order.getSubService().getPrice()) {
+                    Suggestion suggestion = getSuggestion(specialist,suggestedPrice, workHour, startTime, order);
 
-                    Suggestion suggestion = getSuggestion(specialist, suggestedPrice, workHour, startTime, order);
-                    suggestionDao.save(suggestion);
                     order.setOrderState(OrderState.WAITING_FOR_SPECIALIST_SELECTION);
                     orderDao.save(order);
+
+                    suggestion.setSuggestionCode(getRandomCode(suggestionDao));
+                    suggestionDao.save(suggestion);
 
                 } else throw new SuggestedPriceIsHigherThanBasePriceException();
             } else throw new SpecialistNotFoundException();
@@ -70,12 +72,37 @@ public class SuggestionServiceImpl implements SuggestionService {
         else return result.get(0);
     }
 
-    private Suggestion getSuggestion(Specialist specialist, double suggestedPrice, double workHour, Date startTime, Order order) {
+    @Override
+    public List<SuggestionDto> findSuggestionByOrder(String orderCode) {
+        List<Order> resultOrders = orderDao.findOrderByOrderCode(orderCode);
+        if(resultOrders.size()>=1){
+            Order order = resultOrders.get(0);
+            List<Suggestion> suggestions = suggestionDao.findSuggestionByOrderAndSpecialist(order.getId());
+            return suggestions
+                    .stream().map(i-> SuggestionMapper.toSuggestionDto(i,order)).collect(Collectors.toList());
+        }else throw new OrderNotFoundException();
+    }
+
+    private Suggestion getSuggestion(Specialist specialist,double suggestedPrice, double workHour, Date startTime, Order order) {
         return Suggestion.builder()
                 .setSpecialist(specialist)
                 .setSuggestedPrice(suggestedPrice)
                 .setWorkHour(workHour)
                 .setOrder(order)
                 .setStartTime(startTime).build();
+    }
+
+    private String getRandomCode(SuggestionDao suggestionDao) {
+        Random random = new Random();
+        int randomNumber = random.nextInt();
+        if(randomNumber<0){
+            return getRandomCode(suggestionDao);
+        }else {
+            String code = String.valueOf(randomNumber).substring(0,5);
+            int result = suggestionDao.findSuggestionBySuggestionCode(code).size();
+            if (result <= 0) {
+                return code;
+            } else return getRandomCode(suggestionDao);
+        }
     }
 }
--- src/main/java/ir/maktab/service/interfaces/SuggestionService.java
+++ src/main/java/ir/maktab/service/interfaces/SuggestionService.java
@@ -1,16 +1,18 @@
 package ir.maktab.service.interfaces;
 
-import ir.maktab.data.entity.Order;
-import ir.maktab.data.entity.Specialist;
+import ir.maktab.data.dto.SuggestionDto;
 import ir.maktab.data.entity.Suggestion;
 
 import java.util.Date;
+import java.util.List;
 
 public interface SuggestionService {
 
     Suggestion findById(int id);
 
-    void addSpecialistSuggestion(Order order , Specialist specialist, double suggestedPrice , double workHour , Date startTime );
+    void addSpecialistSuggestion(String orderCode, String specialistEmail, double suggestedPrice , double workHour , Date startTime );
 
     Suggestion findBySuggestionCode(String suggestionCode);
+
+    List<SuggestionDto> findSuggestionByOrder(String orderCode);
 }
