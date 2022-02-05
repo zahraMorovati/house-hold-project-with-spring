b933fb7f3d9f4d724a336a8bd34a12e946aeb844
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-02-05 12:49:48 +0330

    add findSuggestionByOrderAndSpecialist & findSpecialistBySuggestionCode in SuggestionDao.java

--- src/main/java/ir/maktab/data/dao/interfaces/SuggestionDao.java
+++ src/main/java/ir/maktab/data/dao/interfaces/SuggestionDao.java
@@ -1,7 +1,9 @@
 package ir.maktab.data.dao.interfaces;
 
 import ir.maktab.data.entity.Suggestion;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,4 +11,10 @@ import java.util.UUID;
 public interface SuggestionDao extends PagingAndSortingRepository<Suggestion,Integer> {
 
     List<Suggestion> findSuggestionBySuggestionCode(String suggestionCode);
+
+    @Query(value = "select * from suggestion where order_id=:orderId",nativeQuery = true)
+    List<Suggestion> findSuggestionByOrderAndSpecialist(@Param("orderId")int orderId);
+
+    @Query(value = "select specialist_id from suggestion where suggestionCode=:suggestionCode",nativeQuery = true)
+    List<Integer> findSpecialistBySuggestionCode(@Param("suggestionCode")String suggestionCode);
 }
