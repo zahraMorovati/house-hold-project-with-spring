f78f325f8e1bfedd6369fc67b5fdf40733e8d033
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-02-11 22:45:05 +0330

    add WebSecurityConfig.java

---
+++ src/main/java/ir/maktab/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package ir.maktab.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@ComponentScan
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/signIn").permitAll()
+                .and()
+                .logout().logoutUrl("/").permitAll();
+    }
+}
