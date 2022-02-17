4d4808ecf9d4891d64b51e499ba90c86c6225979
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2022-02-17 05:35:16 +0330

    modify toOrderState method in Convert.java

--- src/main/java/ir/maktab/util/Convert.java
+++ src/main/java/ir/maktab/util/Convert.java
@@ -16,28 +16,41 @@ public class Convert {
             } catch (ParseException e) {
                 return null;
             }
-        }return null;
+        }
+        return null;
     }
 
-    public static OrderState toOrderState(String state){
-        try {
-            return OrderState.valueOf(state);
-        }catch (Exception e){
-            return null;
+    public static OrderState toOrderState(String state) {
+        if (state == null)
+            return OrderState.NO_STATE;
+        else {
+            if (state.equalsIgnoreCase(OrderState.WAITING_FOR_SPECIALIST_SUGGESTION.toString())) {
+                return OrderState.WAITING_FOR_SPECIALIST_SUGGESTION;
+            } else if (state.equalsIgnoreCase(OrderState.WAITING_FOR_SPECIALIST_SELECTION.toString())) {
+                return OrderState.WAITING_FOR_SPECIALIST_SELECTION;
+            } else if (state.equalsIgnoreCase(OrderState.WAITING_FOR_SPECIALIST_TO_COME.toString())) {
+                return OrderState.WAITING_FOR_SPECIALIST_TO_COME;
+            } else if (state.equalsIgnoreCase(OrderState.DONE.toString())) {
+                return OrderState.DONE;
+            } else if (state.equalsIgnoreCase(OrderState.PAID.toString())) {
+                return OrderState.PAID;
+            } else if (state.equalsIgnoreCase(OrderState.STARTED.toString())) {
+                return OrderState.STARTED;
+            } else return OrderState.NO_STATE;
         }
     }
 
     public static String toHexString(String string) {
         byte[] ba = string.getBytes(StandardCharsets.UTF_8);
         StringBuilder str = new StringBuilder();
-        for(int i = 0; i < ba.length; i++)
+        for (int i = 0; i < ba.length; i++)
             str.append(String.format("%x", ba[i]));
         return str.toString();
     }
 
     public static String fromHexString(String hex) {
         StringBuilder str = new StringBuilder();
-        for (int i = 0; i < hex.length(); i+=2) {
+        for (int i = 0; i < hex.length(); i += 2) {
             str.append((char) Integer.parseInt(hex.substring(i, i + 2), 16));
         }
         return str.toString();
